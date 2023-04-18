d6c6b710151b4a4f9ddd4d6e30252b7998c7b58f
carp22 <carlosrp2207@gmail.com>
2023-04-18 16:40:17 -0400

    Fonts added to CSS

--- index.jsx
+++ index.jsx
@@ -17,7 +17,7 @@ function App() {
       </header>
       
       <Routes>
-        <Route path="/home" element={<Home />}/>
+        <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
         <Route path="/vans" element={<Vans />}/>
       </Routes>
