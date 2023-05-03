e67ebc88e380bf2f82f518f69cba3c8a7df81bf2
carp22 <carlosrp2207@gmail.com>
2023-05-03 16:13:32 -0400

    Added host routes

--- index.jsx
+++ index.jsx
@@ -1,70 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/Vans/VanDetail"
+import Dashboard from "./pages/Host/Dashboard"
+import Income from "./pages/Host/Income"
+import Reviews from "./pages/Host/Reviews"
+import HostVans from "./pages/Host/HostVans"
+import HostVanDetail from "./pages/Host/HostVanDetail"
+import HostVanInfo from "./pages/Host/HostVanInfo"
+import HostVanPricing from "./pages/Host/HostVanPricing"
+import HostVanPhotos from "./pages/Host/HostVanPhotos"
+import Layout from "./components/Layout"
+import HostLayout from "./components/HostLayout"
 
-
+import "./server"
 
 function App() {
   return (
     <BrowserRouter>
-    
-      <header className="vans-header">
-        <Link to="/" className="vanlife-label">#VANLIFE</Link>
-        <section className="vans-navbar">
-          <Link to="/about" className="about-label" >About</Link>
-          <Link to="/vans" className="vans-label"  >Vans</Link>
-        </section>
-      </header>
-      
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/about" element={<About />}/>
-        <Route path="/vans" element={<Vans />}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          
+          <Route path="host" element={<HostLayout />}>
+            <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
+            <Route path="vans" element={<HostVans />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
+          </Route>
+        </Route>
       </Routes>
-      
-      <footer>
-        <h4>Ⓒ 2022 #VANLIFE</h4>
-      </footer>
-      
     </BrowserRouter>
-  )
-}
-function Home() {
-  return (
-    <div>
-      <div className = "vans-body">
-        <h1>You got the travel plans, we got the travel vans.</h1>
-        
-        <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-        
-        <h2>Find your van</h2>
-      </div>
-    </div>
-  )
-}
-function About() {
-  return (
-    <div>
-      <div className="about-body">
-        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
-        
-        <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.(Hitch costs extra 😉)</p>
-        
-        <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
-        
-        <section className="rental-section">
-          <h3>Your destination is waiting. Your van is ready.</h3>
-          <h2>Explore our vans</h2>
-      </section>
-      </div>
-    </div>
-  )
-}
-function Vans(){
-  return(
-    <div>
-      <h2>Vans go here</h2>
-    </div>
   )
 }
 
